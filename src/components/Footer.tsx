dbb14d7bbfa7a513457d27926a0c6907ac32bf09
enzojunqueiraa <enzobjunqueira@gmail.com>
2023-08-31 15:02:31 -0300

    Commit Inicial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import syles from './Footer.module.css'
+
+    const Footer = () =>{
+        return(
+            <footer className={syles.footer}>
+                <p>
+                    <span>Buscador de CEP</span> @SENAI
+                </p>
+
+
+            </footer>
+        )
+    }
+
+    export default Footer;
